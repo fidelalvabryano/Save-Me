dc982b8bf303fd8df34484ebfb3b3c64a286cdfd
Seteven <Seteven2729>
2021-05-26 23:11:52 +0700

    Profile

--- app/src/main/java/com/harasio/savemeapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/harasio/savemeapp/ui/home/HomeFragment.kt
@@ -64,7 +64,7 @@ class HomeFragment : Fragment() {
                         )
                     } != PackageManager.PERMISSION_GRANTED
                 ) {
-                  
+
                     return
                 }
             }
--- app/src/main/java/com/harasio/savemeapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/harasio/savemeapp/ui/profile/ProfileFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.harasio.savemeapp.databinding.FragmentProfileBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import cz.msebera.android.httpclient.Header
+import org.json.JSONArray
+import org.json.JSONObject
 
 class ProfileFragment : Fragment() {
 
-
+    private lateinit var mAuth: FirebaseAuth
     private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
@@ -23,10 +32,43 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
+        getDetailUser()
+    }
+
+    private fun getDetailUser() {
+        val client = AsyncHttpClient()
+        val url = "http://159.65.4.250:3000/api/account/v1/fetch"
+        val params = RequestParams()
+        params.put("_id", mAuth.currentUser?.uid)
+        client.post(url, params ,object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray) {
+                val result = String(responseBody)
+                try {
+                    val responseObject = JSONObject(result)
+                    val dataObject = responseObject.getJSONObject("data")
+
+                    val ausername = dataObject.getString("fullname")
+                    Toast.makeText(context, ausername, Toast.LENGTH_SHORT).show()
+
+                } catch (e: Exception) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                Toast.makeText(context, "GAGAL Fetch!!!", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     override fun onDestroyView() {
